051d25862d196769363da788c2c7c35e29cc0b52
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-07-27 11:55:15 -0300

    feat: implementing migration with FluentMigration

---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DataBaseMigration.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyRecipeBook.Infrastructure.Migrations;
+
+public class DataBaseMigration
+{
+    public static void ExecuteMigrations(IServiceProvider serviceProvider)
+    {
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        runner.ListMigrations();
+        runner.MigrateUp();
+    }
+}
---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DataBaseVersions.cs
@@ -0,0 +1,6 @@
+﻿namespace MyRecipeBook.Infrastructure.Migrations;
+
+internal abstract class DataBaseVersions
+{
+    internal const int TABLE_USERS = 1;
+}
